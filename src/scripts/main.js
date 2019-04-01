c398cf0950aa902b33fbaee48e1fc09ddb7e5a94
Billy Mathison <wlmathison@gmail.com>
2019-04-01 11:45:29 -0500

    deleting unnecessary files and clearing out readme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,23 +3,6 @@
     Name: main.js
     Purpose: Entry point of our application
 */
-// import carFactory from "./createCar"
-// import garage from "./createGarage"
-
-// // Create two cars using the function you imported
-// const mustang = carFactory("Ford", "Mustang")
-// const accord = carFactory("Honda", "Accord")
-// const santafe = carFactory("Hyundai", "Santa Fe")
-// const sierra = carFactory("GMC", "Sierra")
-
-// /// Store the cars in the garage
-// garage.store(mustang)
-// garage.store(accord)
-// garage.store(santafe)
-// garage.store(sierra)
-
-// console.table(garage.inventory())
-// console.table(garage.retrieve(sierra))
 
 import contactList from "./DOMManager"
 import contactForm from "./contactForm"
